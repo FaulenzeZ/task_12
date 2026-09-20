bb7017df4e3f494a16345362e3371a6ca872f93e
FaulenzeZ <idrisov3688066@gmail.com>
2026-09-20 17:19:56 +0700

    Решение

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters;
+class Program
+{
+    static void Main()
+    {
+        string[] mass = Console.ReadLine().Split();
+        int one = int.Parse(mass[0]);
+        int two = int.Parse(mass[1]);
+        int three = int.Parse(mass[2]);
+        int max = one;
+        int min = two;
+        if (two > max) 
+        { 
+            max = two;
+            min = one;
+        }
+        if (three > max) 
+        {
+            max = three; 
+        }
+        else if (three < min)
+        {
+            min = three;
+        }
+
+
+
+        Console.WriteLine($"max = {max}, min ={min}, разница = {max-min}");
+    }
+}
